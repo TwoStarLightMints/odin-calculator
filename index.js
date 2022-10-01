8e6d7d352c9edb669bcf8a8a899a8461379f0fbe
TwoStarLightMints <mondo.beyondo3@gmail.com>
2022-10-01 16:25:54 -0400

    Create operation functions and a function to call them given an operation character

---
+++ index.js
@@ -0,0 +1,34 @@
+function add (num1, num2) {
+  return num1 + num2;
+}
+
+function subtract (num1, num2) {
+  return num1 - num2;
+}
+
+function multiply (num1, num2) {
+  return num1 * num2;
+}
+
+function divide (num1, num2) {
+  if (num2 != 0) {
+    return num1 / num2;
+  }
+}
+
+function operate (oper, num1, num2) {
+  switch (oper) {
+    case '+':
+      add(num1, num2);
+      break;
+    case '-':
+      subtract(num1, num2);
+      break;
+    case '*':
+      multiply(num1, num2);
+      break;
+    case '/':
+      divide(num1, num2);
+      break;
+  }
+}
